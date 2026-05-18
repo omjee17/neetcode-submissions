4dddb63340c6afdd29599b6228bc70bde58bee96
omjee17 <85226502+omjee17@users.noreply.github.com>
2026-05-18 21:41:03 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    
+    bool isPalindrome(string s) {
+        
+        int start=0, end=s.size()-1;
+
+        while(start<=end){
+            while(!isalnum(s[start]) || s[start]==' '){
+                start++;
+            } 
+            while(!isalnum(s[end]) || s[end]==' '){
+                end--;
+            }
+            cout<<s[start]<<" "<<s[end]<<endl;
+
+            if((start<=end) && tolower(s[start])!=tolower(s[end])){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
